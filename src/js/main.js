3db535d766303e03cb872c0f2ae5d95648f8793d
Abdullah <hrfssa@gmail.com>
2018-03-18 16:35:47 -0700

    added faqs page

--- src/js/main.js
+++ src/js/main.js
@@ -346,10 +346,43 @@ $(document).ready(() => {
                     color: '#f2f2f2',
                 },
             ]
+        }), new LineMaker({
+            parent: { element: document.getElementById('faqs-section') },
+            lines: [
+                {
+                    top: '-500px',
+                    left: '15px',
+                    width: 1,
+                    height: '200vh',
+                    color: '#0b0b0b',
+                },
+                {
+                    top: '-500px',
+                    left: '335px',
+                    width: 1,
+                    height: '200vh',
+                    color: '#0b0b0b',
+                },
+                {
+                    top: '-500px',
+                    left: '655px',
+                    width: 1,
+                    height: '200vh',
+                    color: '#0b0b0b',
+                },
+                {
+                    top: '-500px',
+                    left: 'auto',
+                    right: '15px',
+                    width: 1,
+                    height: '200vh',
+                    color: '#0b0b0b',
+                },
+            ]
         }),];
     const myVivus = new Vivus('pathx');
     // start loading X animation
-    myVivus.play(1, async () => {
+    myVivus.play(100, async () => {
         myVivus.reset();
         $('#xtext').fadeIn(1000, () => {
             // fade out of loading screen
@@ -368,6 +401,68 @@ $(document).ready(() => {
             animateNav();
         });
     });
+
+    //uses classList, setAttribute, and querySelectorAll
+//if you want this to work in IE8/9 youll need to polyfill these
+    (function () {
+        var d = document,
+            accordionToggles = d.querySelectorAll('.js-accordionTrigger'),
+            setAria,
+            setAccordionAria,
+            switchAccordion,
+            touchSupported = ('ontouchstart' in window),
+            pointerSupported = ('pointerdown' in window);
+
+        var skipClickDelay = function (e) {
+            e.preventDefault();
+            e.target.click();
+        }
+
+        var setAriaAttr = function (el, ariaType, newProperty) {
+            el.setAttribute(ariaType, newProperty);
+        };
+        setAccordionAria = function (el1, el2, expanded) {
+            switch (expanded) {
+                case "true":
+                    setAriaAttr(el1, 'aria-expanded', 'true');
+                    setAriaAttr(el2, 'aria-hidden', 'false');
+                    break;
+                case "false":
+                    setAriaAttr(el1, 'aria-expanded', 'false');
+                    setAriaAttr(el2, 'aria-hidden', 'true');
+                    break;
+                default:
+                    break;
+            }
+        };
+//function
+        switchAccordion = function (e) {
+            console.log("triggered");
+            e.preventDefault();
+            var thisAnswer = e.target.parentNode.nextElementSibling;
+            var thisQuestion = e.target;
+            if (thisAnswer.classList.contains('is-collapsed')) {
+                setAccordionAria(thisQuestion, thisAnswer, 'true');
+            } else {
+                setAccordionAria(thisQuestion, thisAnswer, 'false');
+            }
+            thisQuestion.classList.toggle('is-collapsed');
+            thisQuestion.classList.toggle('is-expanded');
+            thisAnswer.classList.toggle('is-collapsed');
+            thisAnswer.classList.toggle('is-expanded');
+
+            thisAnswer.classList.toggle('animateIn');
+        };
+        for (var i = 0, len = accordionToggles.length; i < len; i++) {
+            if (touchSupported) {
+                accordionToggles[i].addEventListener('touchstart', skipClickDelay, false);
+            }
+            if (pointerSupported) {
+                accordionToggles[i].addEventListener('pointerdown', skipClickDelay, false);
+            }
+            accordionToggles[i].addEventListener('click', switchAccordion, false);
+        }
+    })();
 });
 
 
@@ -408,3 +503,4 @@ $.fn.extend({
         return this;
     },
 });
+
